47d45a2ce55eee0ad47ad173b4c219253b2e9f42
FagionatoRafael <67435581+FagionatoRafael@users.noreply.github.com>
2021-11-01 14:43:46 -0300

    add mongodb

--- config.js
+++ config.js
@@ -1,16 +1,14 @@
-const {env} = require('process')
+let envs;
 
-// let envs;
+if (process.env.DBNAME !== 'production'){ 
+    const dotenv = require('dotenv'); 
 
-// if (process.env.DBNAME !== 'production'){ 
-//     const dotenv = require('dotenv'); 
-
-//     const result = dotenv.config();
-//     envs = result.parsed;
-// } else {
-//     const _ = require('lodash');
-//     envs = {};
-//     _.each(process.env, (value, key) => envs[key] = value);
-// }
-console.log(env)
-// module.exports = envs;
+    const result = dotenv.config();
+    envs = result.parsed;
+} else {
+    const _ = require('lodash');
+    envs = {};
+    _.each(process.env, (value, key) => envs[key] = value);
+}
+console.log(process.env)
+module.exports = envs;
