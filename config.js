08cdd13efaa6c6e941f051ed2439bf393272300f
FagionatoRafael <67435581+FagionatoRafael@users.noreply.github.com>
2021-11-01 11:35:59 -0300

    add mongodb

--- config.js
+++ config.js
@@ -1,15 +1,18 @@
-const dotenv = require('dotenv');
-const _ = require('lodash');
-
-const result = dotenv.config();
-
+const PORT = process.env.PORT || 8080;
 let envs;
 
-if (!('error' in result)) {
-  envs = result.parsed;
+if(PORT === 8080) {
+    const dotenv = require('dotenv');
+    const result = dotenv.config();
+
+    if (!('error' in result)) {
+        envs = result.parsed;
+    }
 } else {
-  envs = {};
-  _.each(process.env, (value, key) => envs[key] = value);
+    const _ = require('lodash');
+    
+    envs = {};
+    _.each(process.env, (value, key) => envs[key] = value);
 }
 
 module.exports = envs;
