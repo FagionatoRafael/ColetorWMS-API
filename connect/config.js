ca508f0503908fc351e90e6e89b035ac19a0552b
FagionatoRafael <67435581+FagionatoRafael@users.noreply.github.com>
2021-11-01 11:14:21 -0300

    add mongodb

--- connect/config.js
+++ connect/config.js
@@ -1,18 +1,19 @@
 const { MongoClient } = require('mongodb');
+const envs = require('../config')
 const PORT = process.env.PORT || 8080;
 if(PORT === 8080) {
     require('dotenv').config()
 }
  
 function Collection() {
-    const url = process.env.URL || '';
+    const url = envs.URL || '';
     const client = new MongoClient(url);
 
-    const dbName = process.env.DBNAME || '';
+    const dbName = envs.DBNAME || '';
     client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
-    const collection = db.collection(process.env.COLLECTIONNAME || '');
+    const collection = db.collection(envs.COLLECTIONNAME || '');
     return collection
 }
 
